104c57e0bf6f054aeb162ec4a0b68c9b2020650a
BasePC <jy0001087@gmail.com>
2024-09-08 21:55:00 +0800

    颠倒x y轴

--- Test.py
+++ Test.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_excel('23总结.xlsx')
-branch = '福州分行'
+branch = '成都分行'
 
 filter_df = df[df['主办分行'] == branch]
 
@@ -16,39 +16,24 @@ plt.rcParams['font.sans-serif'] = ['SimHei']
 x = result_df.index
 y = result_df.values
 
-# 处理 x 轴标签，每行显示两个中文
-formatted_x = []
-for label in x:
-    words = label.split()
-    new_label = []
-    line = ""
-    for word in words:
-        if len(line) + len(word) < 4:  # 假设每个中文字符占 2 个单位
-            line += word + " "
-        else:
-            new_label.append(line.strip())
-            line = word + " "+"\n"
-    new_label.append(line.strip())
-    formatted_x.append("\n".join(new_label))
 
-
-plt.bar(formatted_x,y)
+plt.barh(x,y)
 
 # 计算日均存款值的汇总数
 total_balance = sum(y)
 
 for i, v in enumerate(y):
     percentage = v / total_balance * 100
-    plt.text(i, v + 0.1, f"{v:.2f} ({percentage:.2f}%)", ha='center') 
+    plt.text(v + 1,i,f"{v:.2f} ({percentage:.2f}%)", ha='left') 
 
 
 
 # 添加标题和坐标轴标签
 plt.title(branch+'存款柱状图'+'         '+f"机构存款总日均: {total_balance:.2f}")
-plt.xlabel('行业')
-plt.ylabel('日均存款（亿元）')
+plt.xlabel('日均存款（亿元）')
+plt.ylabel('行业')
 #用于旋转 x 轴标签以更好地显示
-plt.xticks(rotation=90) 
+plt.xticks(rotation=60) 
 
 # 显示图形
 plt.show()
